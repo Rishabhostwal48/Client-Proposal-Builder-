06bbf9b608dc8061b16339e5fdc9bee5efb67c12
Rishabh Ostwal <ostwalrishabh0817@gmail.com>
2026-06-21 03:14:29 +0530

    started server

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,15 @@
-import "Express" from "express"
-import "dotenv"
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
