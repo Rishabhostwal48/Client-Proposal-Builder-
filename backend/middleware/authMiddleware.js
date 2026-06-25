b32d92abd8b601675dc1201195a80f955caffb30
Rishabh Ostwal <ostwalrishabh0817@gmail.com>
2026-06-26 00:55:58 +0530

    Auth middleware crated to protect

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const protect = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Not authorized, token failed",
+      })
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Not authorized, user not found",
+      })
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(500).json({
+        message: error.message
+    })
+  }
+};
+
+module.exports = { protect };
