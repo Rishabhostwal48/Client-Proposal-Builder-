2573981348df7c6bbce855bb95544b5317392166
Rishabh Ostwal <ostwalrishabh0817@gmail.com>
2026-06-29 02:38:51 +0530

    fix the error with getProposalById

--- backend/controllers/proposalController.js
+++ backend/controllers/proposalController.js
@@ -23,48 +23,57 @@ const createProposal = async (req, res) => {
     });
 
     res.status(201).json({
-        message:"Proposal creaated successfully",
-        proposal
-    })
+      message: "Proposal creaated successfully",
+      proposal,
+    });
   } catch {
-       res.status(500).json({
-        message: error.message
+    res.status(500).json({
+      message: error.message,
     });
   }
-  
 };
 
-
-const getProposals = async(req,res)=>{
-   try {Proposal.find({user:req.user._id}).then((proposals)=>{
-        res.status(200).json({
-            message:"Proposals fetched successfully",
-            proposals
-        })
-    })}catch(error){
-      res.status(500).json({
-        message:error.message,
-      })
-    }
-}
-
-const getProposalById = async(req,res)=>{
-  try{
-    Proposal.findById(req.params.id).then((proposal)=>{
-      req.user._id.toString()===proposal.user.toString()?res.status(200).json({
-        message:"Proposal fetched successfully",
-        proposal
-      }):res.status(403).json({
-        message:"You are not authorized to view this proposal"
-      })
-    })
-  }catch(error){
+const getProposals = async (req, res) => {
+  try {
+    Proposal.find({ user: req.user._id }).then((proposals) => {
+      res.status(200).json({
+        message: "Proposals fetched successfully",
+        proposals,
+      });
+    });
+  } catch (error) {
     res.status(500).json({
-      message:error.message
-    })
-
+      message: error.message,
+    });
   }
-}
+};
+
+const getProposalById = async (req, res) => {
+  try {
+    const proposal = await Proposal.findById(req.params.id);
+
+    if (!proposal) {
+      return res.status(404).json({
+        message: "Proposal not found",
+      });
+    }
+    const isUser = req.user._id.toString() === proposal.user.toString();
+    if (isUser) {
+      return res.status(200).json({
+        message: "Proposal fetched successfully",
+        proposal,
+      });
+    } else {
+      return res.status(403).json({
+        message: "You are not authorized to view this proposal",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {
   createProposal,
