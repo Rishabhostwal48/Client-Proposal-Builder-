fbcb6c59a0f76f9480a00aeed94264e8d9e787cd
Rishabh Ostwal <ostwalrishabh0817@gmail.com>
2026-06-30 22:20:00 +0530

    added function to update proposal

--- backend/controllers/proposalController.js
+++ backend/controllers/proposalController.js
@@ -75,8 +75,41 @@ const getProposalById = async (req, res) => {
   }
 };
 
+const updateProposal = async (req,res) =>{
+  try {
+    const proposal = await Proposal.findById(req.params.id);
+
+    if (!proposal) {
+      return res.status(404).json({
+        message: "Proposal not found",
+      });
+    }
+    const isUser = req.user._id.toString() === proposal.user.toString();
+    if (isUser) {
+      const updatedProposal = await Proposal.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true, runValidators: true }
+      );
+      return res.status(200).json({
+        message: "Proposal updated successfully",
+        proposal: updatedProposal,
+      });
+    } else {
+      return res.status(403).json({
+        message: "You are not authorized to view this proposal",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+}
+
 module.exports = {
   createProposal,
   getProposals,
   getProposalById,
+  updateProposal,
 };
--- backend/routes/proposalRoute.js
+++ backend/routes/proposalRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {protect} = require('../middleware/authMiddleware');
-const {createProposal,getProposals,getProposalById} = require('../controllers/proposalController');
+const {createProposal,getProposals,getProposalById,updateProposal} = require('../controllers/proposalController');
 
 router.post('/create', protect, createProposal);
 
 router.get('/list',protect, getProposals);
 router.get('/:id',protect, getProposalById);
+router.put('/:id',protect, updateProposal); 
 
 module.exports = router;
