d1bbad439d68a26e9df63c66d2b1209cd6a0c6b2
Rishabh Ostwal <ostwalrishabh0817@gmail.com>
2026-07-05 01:02:47 +0530

    learning to handle the form in frontend

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import { useState } from "react";
 
-const register = () => {
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    console.log(e);
+  };
   return (
     <form>
-        Name:<input type="text" name="name" required />
-        Email:<input type="email" name="email" required />
-        Password:<input type="password" name="password" required />
-        <button type="submit">Register</button>
+      Name:
+      <input type="text" name="name" required onChange={handleChange} />
+      Email:
+      <input type="email" name="email" required />
+      Password:
+      <input type="password" name="password" required />
+      <button type="submit">Register</button>
     </form>
-)
-}
+  );
+};
 
-export default register
+export default Register;
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,10 @@
-const axios = require('axios');
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
-})
- 
+    baseURL: "http://localhost:5000/api",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+export default api;
