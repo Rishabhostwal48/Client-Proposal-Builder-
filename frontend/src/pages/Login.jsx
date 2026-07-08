d32da8b4a3fa09f457e531a7eb34e4958395ecd7
Rishabh Ostwal <ostwalrishabh0817@gmail.com>
2026-07-08 21:14:57 +0530

    Build and connect Login page and now you can login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import { useState } from "react";
+import api from "../services/api"
+import {useNavigate} from "react-router-dom"
 
-const login = () => {
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    const response = await api.post("auth/login", formData);
+    console.log(response.data);
+    localStorage.setItem("token", response.data.token);
+    
+    navigate("/dashboard");
+  };
   return (
-    <form >
-      Email:<input type="email" name="email" required/>
-      Password:<input type="password" name="password" required/>
+    <form onSubmit={handleSubmit}>
+      Email:
+      <input type="email" name="email" required onChange={handleChange} />
+      Password:
+      <input type="password" name="password" required onChange={handleChange} />
       <button type="submit">Login</button>
     </form>
-  )
-}
+  );
+};
 
-export default login
+export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -10,27 +10,24 @@ const Register = () => {
 
   const handleChange = (e) => {
     setFormData({
-    ...formData,
-    [e.target.name]: e.target.value,
-  });
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(formData);
-    const response = await api.post(
-  "/auth/register",
-  formData
-);
-     console.log(response.data);
-  }
+    const response = await api.post("/auth/register", formData);
+    console.log(response.data);
+  };
   return (
     <form onSubmit={handleSubmit}>
       Name:
       <input type="text" name="name" required onChange={handleChange} />
       Email:
-      <input type="email" name="email" required onChange={handleChange}/>
+      <input type="email" name="email" required onChange={handleChange} />
       Password:
-      <input type="password" name="password" required onChange={handleChange}/>
+      <input type="password" name="password" required onChange={handleChange} />
       <button type="submit">Register</button>
     </form>
   );
