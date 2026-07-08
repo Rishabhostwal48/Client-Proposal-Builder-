98e967a0e0e424096277184e70a9014048337bbf
Rishabh Ostwal <ostwalrishabh0817@gmail.com>
2026-07-08 17:25:23 +0530

    Fixed error when the backend is not giving the access to enter data through frontend

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const proposalRoutes = require("./routes/proposalRoute");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/proposal",proposalRoutes)
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {api} from "../services/api";
+import api from "../services/api";
 
 const Register = () => {
   const [formData, setFormData] = useState({
